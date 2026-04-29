4e8da79311b99a09624eef00d3b1b16bea74f6bd
Valeriy Semenik <valeriy.semenik@gmail.com>
2026-04-29 14:41:34 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,8 +1,7 @@
-import datetime
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .models import Driver, Manufacturer, Car
+from taxi.models import Driver, Manufacturer, Car
 
 
 def index(request: HttpRequest) -> HttpResponse:
