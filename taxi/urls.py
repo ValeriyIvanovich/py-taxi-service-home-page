59118d7bab96b5cc2f5d8d74d1fe1f10496839c8
Valeriy Semenik <valeriy.semenik@gmail.com>
2026-04-29 17:19:32 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from taxi.views import index
 
-
 urlpatterns = [
     path("", index, name="index"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
